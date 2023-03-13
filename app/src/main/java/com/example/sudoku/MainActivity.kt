5caeccddccc4c7f02122a3a62f53651a64e1c9c8
Mohamed Naser <mahmmedn19@gmail.com>
2023-03-14 00:42:49 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sudoku/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.sudoku
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.InputType
+import android.view.Gravity
+import android.widget.EditText
+import android.widget.GridLayout
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    }
+
