1fc5fd4e59103f39ac4d5296b1d2d2caf3ec2b1a
Mohamed Naser <mahmmedn19@gmail.com>
2023-03-14 18:21:54 +0200

    implement sudoku code and refactor design

--- app/src/main/java/com/example/sudoku/MainActivity.kt
+++ app/src/main/java/com/example/sudoku/MainActivity.kt
@@ -33,24 +33,118 @@ class MainActivity : AppCompatActivity() {
             val button = findViewById<Button>(buttonId)
             button.setOnClickListener { onNumberButtonClick(i) }
         }
-        createCells()
+        // Initialize empty Sudoku grid
+        val grid = Array(numRows) { IntArray(numCols) }
+
+        createCells(grid)
 
         // Set listeners for solve, clear, and clear all buttons
-        btnSolve.setOnClickListener { /*solveSudoku()*/ }
+        btnSolve.setOnClickListener {
+            val grid = getCurrentGridState()
+            // Check if at least 10 cells are filled
+            var numFilledCells = 0
+            for (row in 0 until numRows) {
+                for (col in 0 until numCols) {
+                    if (grid[row][col] != 0) {
+                        numFilledCells++
+                    }
+                }
+            }
+            if (numFilledCells < 10) {
+                Toast.makeText(this, "Please fill in at least 10 cells before solving", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (getSudokuSolution(grid)) {
+                updateGrid(grid)
+            } else {
+                Toast.makeText(this, "Unable to solve Sudoku", Toast.LENGTH_SHORT).show()
+            }
+        }
         btnClear.setOnClickListener { clearSelectedCell() }
         btnClearAll.setOnClickListener { clearAllCells() }
     }
+    private fun getCurrentGridState(): Array<IntArray> {
+        val gridState = Array(numRows) { IntArray(numCols) }
 
-    private fun createCells() {
         for (row in 0 until numRows) {
             for (col in 0 until numCols) {
-                val cell = createCell(row, col)
+                val cell = gridLayout.getChildAt(row * numCols + col) as TextView
+                val cellValue = cell.text.toString().toIntOrNull() ?: 0
+                gridState[row][col] = cellValue
+            }
+        }
+
+        return gridState
+    }
+    private fun getSudokuSolution(grid: Array<IntArray>): Boolean {
+        // Find the first empty cell
+        var row = -1
+        var col = -1
+        for (r in 0 until numRows) {
+            for (c in 0 until numCols) {
+                if (grid[r][c] == 0) {
+                    row = r
+                    col = c
+                    break
+                }
+            }
+            if (row != -1) {
+                break
+            }
+        }
+
+        // If there are no empty cells, the grid is already solved
+        if (row == -1) {
+            return true
+        }
+
+        // Try all possible values for the current cell
+        for (num in 1..9) {
+            if (isValidMove(grid, row, col, num)) {
+                grid[row][col] = num
+                if (getSudokuSolution(grid)) {
+                    return true
+                }
+                grid[row][col] = 0
+            }
+        }
+
+        // If no value works for the current cell, backtrack
+        return false
+    }
+
+    private fun isValidMove(grid: Array<IntArray>, row: Int, col: Int, num: Int): Boolean {
+        // Check row and column for conflicts
+        for (i in 0 until numCols) {
+            if (grid[row][i] == num || grid[i][col] == num) {
+                return false
+            }
+        }
+
+        // Check 3x3 box for conflicts
+        val boxRow = row - row % 3
+        val boxCol = col - col % 3
+        for (r in boxRow until boxRow + 3) {
+            for (c in boxCol until boxCol + 3) {
+                if (grid[r][c] == num) {
+                    return false
+                }
+            }
+        }
+
+        // If no conflicts found, the move is valid
+        return true
+    }
+    private fun createCells(grid: Array<IntArray>) {
+        for (row in 0 until numRows) {
+            for (col in 0 until numCols) {
+                val cell = createCell(row, col, grid)
                 gridLayout.addView(cell)
             }
         }
     }
 
-    private fun createCell(row: Int, col: Int): TextView {
+    private fun createCell(row: Int, col: Int, grid: Array<IntArray>): TextView {
         val cell = TextView(this)
         val params = GridLayout.LayoutParams()
         params.width = cellSize
@@ -71,20 +165,31 @@ class MainActivity : AppCompatActivity() {
         cell.gravity = Gravity.CENTER
         cell.setTextColor(ContextCompat.getColor(this, R.color.black))
         cell.setBackgroundResource(R.drawable.cell_border)
-        cell.setOnClickListener {
-            // Toggle the cell background and update its text on click
-            // Clear the selection of the previously selected cell (if any)
-            selectedCell?.isSelected = false
-            selectedCell?.setBackgroundResource(R.drawable.cell_border)
+        val value = grid[row][col]
+        if (value != 0) {
+            cell.text = value.toString()
+            cell.isClickable = false
+            cell.setBackgroundColor(ContextCompat.getColor(this, R.color.teal_200))
+        } else {
+            cell.setOnClickListener {
+                // Toggle the cell background and update its text on click
+                // Clear the selection of the previously selected cell (if any)
+                selectedCell?.isSelected = false
+                selectedCell?.setBackgroundResource(R.drawable.cell_border)
 
-            // Set the selected cell and change its background color
-            selectedCell = cell
-            cell.isSelected = true
-            cell.setBackgroundColor(ContextCompat.getColor(this, R.color.purple_500))
+                // Set the selected cell and change its background color
+                selectedCell = cell
+                cell.isSelected = true
+                cell.setBackgroundColor(ContextCompat.getColor(this, R.color.purple_500))
+            }
         }
         return cell
     }
 
+    private fun updateGrid(grid: Array<IntArray>) {
+        gridLayout.removeAllViews()
+        createCells(grid)
+    }
     private fun onNumberButtonClick(num: Int) {
         // Set the text of the selected cell to the input value (if a cell is selected)
         selectedCell?.let {
